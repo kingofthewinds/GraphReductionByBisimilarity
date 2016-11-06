b9ca5708903fe15ca70ac2a04e4fdaaf390a0d32
kingwinds <m.k.winds@gmail.com>
2016-11-06 22:23:17 +0100

    mpi send wrapper implemented using the nonblocking openmpi send function

--- Src/cluster/cluster.cpp
+++ Src/cluster/cluster.cpp
@@ -8,6 +8,21 @@ Cluster::Cluster()
 	MPI_Comm_rank(MPI_COMM_WORLD, &rankOfCurrentNode);
 }
 
+//A non-blocking send 
+//dataSize is the total size of the data (if a vactor then vector * size of each element)
+MPI_Request* Cluster::send(int destination,tags tag, const void *buf,int dataSize,MPI_Datatype datatype)
+{
+	MPI_Request* request = new MPI_Request;
+	MPI_Isend(buf , dataSize , datatype, destination , tag, MPI_COMM_WORLD,request);
+	return request;
+}
+
+//waits for a non-blocking send to finish 
+void Cluster::waitForSend(MPI_Request *request)
+{
+	MPI_Wait(request, MPI_STATUS_IGNORE);
+}
+
 ClusterHandler& ClusterHandler::operator=(const ClusterHandler& rhs)
 {
 	++*rhs.refptr;
